b4dccb3a2a4eeb8ee7946be06165aae18e7eed93
Renan <renan.lima90@gmail.com>
2020-07-28 23:41:19 -0300

    Commited the first stage of the project (after 1st live): basic FPS controlls working

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float Sensitivity;
+    public float Smoothing;
+
+    public float YHighLimit;
+    public float YLowLimit;
+
+    private Vector2 look;
+    private Vector2 smooth;
+
+    private Vector2 scaleVector;
+
+    private Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scaleVector = new Vector2(Sensitivity * Smoothing, Sensitivity * Smoothing);
+
+        playerTransform = transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mouseMov = GetMouseMovement();
+
+        mouseMov = Vector2.Scale(mouseMov, scaleVector);
+
+        smooth.x = Mathf.Lerp(smooth.x, mouseMov.x, 1 / Smoothing);
+        smooth.y = Mathf.Lerp(smooth.y, mouseMov.y, 1 / Smoothing);
+
+        look += smooth;
+
+        look.y = Mathf.Clamp(look.y, -YLowLimit, YHighLimit);
+
+        transform.localRotation = Quaternion.AngleAxis(-look.y, Vector3.right);
+
+        playerTransform.localRotation = Quaternion.AngleAxis(look.x, playerTransform.up);
+    }
+
+    private Vector2 GetMouseMovement()
+    {
+        return new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxis("Mouse Y"));
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+    public float JumpForce;
+
+    private Vector3 movementDirection;
+
+    private Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputSide = Input.GetAxis("Horizontal");
+        float inputForward = Input.GetAxis("Vertical");
+
+        Vector3 sideMovement = transform.right;
+        sideMovement *= (inputSide * Speed * Time.deltaTime);
+        
+        Vector3 forwardMovement = transform.forward;
+        forwardMovement *= (inputForward * Speed * Time.deltaTime);
+
+        movementDirection = sideMovement + forwardMovement;
+
+        transform.position += movementDirection;
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            // Faça o personagem pular
+            rb.AddForce(Vector3.up * JumpForce);
+        }
+
+    }
+}
